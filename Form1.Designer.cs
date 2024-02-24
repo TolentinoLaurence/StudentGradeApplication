bca86474d501d0595e9afa6c7389c1997d777be0
Tolentino.249506 <Tolentino.249506@LAB-A-PC21>
2024-02-24 16:11:57 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace StudentGradeApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtSI = new System.Windows.Forms.TextBox();
+            this.txtIT = new System.Windows.Forms.TextBox();
+            this.txtQT = new System.Windows.Forms.TextBox();
+            this.txtTN = new System.Windows.Forms.TextBox();
+            this.txtPC = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(216, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(324, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DI MASELAYAN COLLEGE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NAME:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(59, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "GRADE";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(50, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "SYSTEM INTEGRATION";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(50, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "INFORMATION TECH";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(50, 275);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "QUANTITATIVE";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(50, 304);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "TECHNOPRENEUR";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(50, 333);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(122, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "PHILPOP CULTURE";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(108, 115);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(311, 21);
+            this.textBox1.TabIndex = 8;
+            // 
+            // txtSI
+            // 
+            this.txtSI.Location = new System.Drawing.Point(202, 211);
+            this.txtSI.Name = "txtSI";
+            this.txtSI.Size = new System.Drawing.Size(80, 20);
+            this.txtSI.TabIndex = 9;
+            this.txtSI.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtIT
+            // 
+            this.txtIT.Location = new System.Drawing.Point(202, 242);
+            this.txtIT.Name = "txtIT";
+            this.txtIT.Size = new System.Drawing.Size(80, 20);
+            this.txtIT.TabIndex = 10;
+            this.txtIT.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtQT
+            // 
+            this.txtQT.Location = new System.Drawing.Point(202, 271);
+            this.txtQT.Name = "txtQT";
+            this.txtQT.Size = new System.Drawing.Size(80, 20);
+            this.txtQT.TabIndex = 11;
+            this.txtQT.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtTN
+            // 
+            this.txtTN.Location = new System.Drawing.Point(202, 301);
+            this.txtTN.Name = "txtTN";
+            this.txtTN.Size = new System.Drawing.Size(80, 20);
+            this.txtTN.TabIndex = 12;
+            this.txtTN.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtPC
+            // 
+            this.txtPC.Location = new System.Drawing.Point(202, 333);
+            this.txtPC.Name = "txtPC";
+            this.txtPC.Size = new System.Drawing.Size(80, 20);
+            this.txtPC.TabIndex = 13;
+            this.txtPC.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 39);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "COMPUTE GENERAL AVERAGE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotal.Location = new System.Drawing.Point(519, 215);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(71, 22);
+            this.txtTotal.TabIndex = 15;
+            this.txtTotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(440, 218);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(73, 15);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "AVERAGE:";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.BackColor = System.Drawing.SystemColors.GrayText;
+            this.txtMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMessage.Location = new System.Drawing.Point(373, 276);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(316, 15);
+            this.txtMessage.TabIndex = 17;
+            this.txtMessage.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(724, 474);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPC);
+            this.Controls.Add(this.txtTN);
+            this.Controls.Add(this.txtQT);
+            this.Controls.Add(this.txtIT);
+            this.Controls.Add(this.txtSI);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtSI;
+        private System.Windows.Forms.TextBox txtIT;
+        private System.Windows.Forms.TextBox txtQT;
+        private System.Windows.Forms.TextBox txtTN;
+        private System.Windows.Forms.TextBox txtPC;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtMessage;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentGradeApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double SI, IT, QT, TN, PC;
+            string Total, MSG;
+
+            SI = int.Parse(txtSI.Text);
+            IT = int.Parse(txtIT.Text);
+            QT = int.Parse(txtQT.Text);
+            TN = int.Parse(txtTN.Text);
+            PC = int.Parse(txtPC.Text);
+
+            double total = (SI + IT + QT + TN + PC) / 5;
+            txtTotal.Text = total.ToString();
+
+            if (total > 75)
+            {
+                string msg = ("Keep it up");
+                txtMessage.Text = msg;
+            } 
+            else if (total < 75) {
+                string msg = ("Lumayas ka na");
+                txtMessage.Text = msg;
+            } else {
+                string msg = ("Invalid Input...");
+                txtMessage.Text = msg;
+            }
+
+        }
+    }
+}
